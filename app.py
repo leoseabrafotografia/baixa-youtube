686a194bac424b7d80233665b1acd661f45d2ec3
leoseabrafotografia <contato@leoseabrafotografia.com>
2025-04-04 17:58:42 -0300

    app.py

--- app.py
+++ app.py
@@ -46,5 +46,6 @@ def index():
 def download_file(filename):
     return send_from_directory(DOWNLOAD_FOLDER, filename, as_attachment=True)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
